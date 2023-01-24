1f12ea1c3149a0cc68f39f4b5b3e913af0cd2734
SerhiyDukh1984 <s.dux2011@ukr.net>
2023-01-24 15:37:40 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
     <div className={s.a}>
       <section className={s.section}>
         <h1 className={s.title}>--- Hello ---</h1>
-        <p className={s.text}>I hope I coped with the task</p>
       </section>
       <Footer />
     </div>
